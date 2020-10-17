ada2375ef0df8319bd1f69d4434173a8d42b2a94
Roberto Valdez <robert.tito94@gmail.com>
2020-10-17 15:33:28 -0500

    added docs

---
+++ docs/precache-manifest.a46159ddb73e051de0b0d1afb63b78a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8a7da3eca5b0fc1d10e40c16886b51f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "872edfd63593fdd6e385",
+    "url": "/static/css/main.37ce8fa7.chunk.css"
+  },
+  {
+    "revision": "580a77c3540062b34f99",
+    "url": "/static/js/2.53780636.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.53780636.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "872edfd63593fdd6e385",
+    "url": "/static/js/main.70879bb4.chunk.js"
+  },
+  {
+    "revision": "4e3a1f73b6e5de40b557",
+    "url": "/static/js/runtime-main.5c054f69.js"
+  }
+]);
