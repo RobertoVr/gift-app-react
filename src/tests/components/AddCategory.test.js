4b4dea9f720bb80c73ae31468b5e815da66850f3
Roberto Valdez <robert.tito94@gmail.com>
2020-10-20 14:06:35 -0500

    add categories tests

--- src/tests/components/AddCategory.test.js
+++ src/tests/components/AddCategory.test.js
@@ -27,7 +27,14 @@ describe('Pruebas eb <AddCategory />', () => {
         wrapper.find('form').simulate('submit', { preventDefault() { } });
         expect(setCategories).not.toHaveBeenCalled();
     });
-
-
+    test('Debe de llamar el setCategories y limpiar la caja de texto', () => {
+        const value = 'One punch man';
+        wrapper.find('input').simulate('change', { target: { value } });
+        wrapper.find('form').simulate('submit', { preventDefault() { } });
+        expect(setCategories).toHaveBeenCalled();
+        expect(setCategories).toHaveBeenCalledTimes(1);
+        expect(setCategories).toHaveBeenCalledWith(expect.any(Function));
+        expect(wrapper.find('input').prop('value')).toBe('');
+    });
 
 });
